055bbab22df2b7c4de0f4cfad4c42075bf676a4d
Lucien <zhouhao2@hisense.com>
2017-07-16 12:03:52 +0800

    start MNIST next step : finish MNIST

---
+++ tensorflowwithpy2/basicusage.py
@@ -0,0 +1,15 @@
+#!/home/zhouhao2/tensorflow/bin/python
+import tensorflow as tf
+
+
+matrix1 = tf.constant([[3., 3.]])
+
+
+matrix2 = tf.constant([[2.],[2.]])
+
+
+product = tf.matmul(matrix1, matrix2)
+
+with tf.Session() as sess:
+	result = sess.run(product)
+	print result
---
+++ tensorflowwithpy2/intreractiveSession.py
@@ -0,0 +1,11 @@
+#!/home/zhouhao2/tensorflow/bin/python
+import tensorflow as tf
+sess = tf.InteractiveSession()
+
+x = tf.Variable([1.0, 2.0])
+a = tf.constant([3.0, 3.0])
+
+x.initializer.run()
+
+sub = tf.sub(x, a)
+print sub.eval()
